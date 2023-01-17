abfad15f8d5cb96a77b7083ebc3d4b547f527e0a
davide <davidemaria.colmi@gmail.com>
2023-01-17 09:07:19 +0100

    first commit

---
+++ src/app/shopping-components/shoppingList/shopping-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
+})
+export class ShopingListComponent implements OnInit {
+    ngOnInit(): void {
+    }
+
+    constructor() {}    
+}
