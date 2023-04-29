a7b76a1691408202a65371e05514c0f88bf545fe
davide <davidemaria.colmi@gmail.com>
2023-04-29 09:38:50 +0200

    morning study

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -13,14 +13,16 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   recipeSelected = new Subject<Recipe>();
   
-  private recipes: Recipe[] = [
+/*   private recipes: Recipe[] = [
     new Recipe('Recipe Test Title', 'Recipe description test', 
     'https://www.negroni.com/sites/negroni.com/files/styles/scale__1440_x_1440_/public/panini_farciti.jpg?itok=kewT82jY', 
     [new Ingredient("pane", 2), new Ingredient("carne", 1)]),
     new Recipe('Carbonara', 'Recipe2 description test', 
     'https://blog.giallozafferano.it/albe/wp-content/uploads/2020/08/15FA1142-B5FA-410C-878B-2B8745B85F64.jpeg', 
     [new Ingredient("cotoletta", 2), new Ingredient("partatine", 20)])
-  ];
+  ]; */
+
+  private recipes: Recipe[] = [];
 
   constructor() { }
 
@@ -47,6 +49,11 @@ export class RecipeService {
     this.recipesChanged.next(this.recipes.slice());
   }
 
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice())
+  }
+
 }
 
 export const recipeResolver: ResolveFn<Observable<Recipe> | Promise<Recipe> | Recipe> = 
@@ -54,4 +61,4 @@ export const recipeResolver: ResolveFn<Observable<Recipe> | Promise<Recipe> | Re
   console.log('recipeResolver');  
   return inject(RecipeService).recipeSelected;
 }
- 
+
